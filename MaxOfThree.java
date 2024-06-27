5407f5c90dafb42ec0c1354e1e11ab6e1edebc36
Gaurav <gpdhande12@gmail.com>
2024-06-27 17:46:38 +0530

    compare 3 integers

--- MaxOfThree.java
+++ MaxOfThree.java
@@ -2,52 +2,45 @@ package org.genericus;
 
 public class MaxOfThree 
 {
-		private static String a;
-		private static String b;
-		private static String c;
+		private static Integer a;
+		private static Integer b;
+		private static Integer c;
 		
 		
-//		 public static Float MaximumOfThree(float num1,float num2,float num3)
-//		 {
-//			float c = Math.max(Math.max(num1, num2), num3);
-//			return c;
-//		 }
-		
-		public static String MaximumOfThreeString(String a, String b, String c) 
+		public static Integer MaximumOfThree(Integer a,Integer b, Integer c)
 		{
-	       String max = a;
-	       if(b.compareTo(max)>0)
-	       {
-	    	   max =b;
-	       }
-	       if(c.compareTo(max)>0)
-	       {
-	    	   max = c;
-	       }
-	       return max;   
-	    }
-		
+			Integer max =a;
+			if(b.compareTo(max)>0)
+			{
+				max = b;
+			}
+			if(c.compareTo(max)>0)
+			{
+				max =c;
+			}
+			return max;	
+		}
 		
 		public static void main(String[] args) 
 		{
-			// Test case 1: Max string at 1st position
-	        String a = "Peach";
-	        String b = "Apple";
-	        String c = "Banana";
-	        System.out.println("Maximum of \"" + a + "\", \"" + b + "\", \"" + c + "\" is \"" + MaximumOfThreeString(a, b, c) + "\"");
+			// Test case 1: Max number at 1st position
+	        Integer a = 10;
+	        Integer b = 5;
+	        Integer c = 3;
+	        System.out.println("Maximum of " + a + ", " + b + ", " + c + " is " + MaximumOfThree(a, b, c));
+	        // Test case 2: Max number at 2nd position
+	        a = 5;
+	        b = 10;
+	        c = 3;
+	        System.out.println("Maximum of " + a + ", " + b + ", " + c + " is " + MaximumOfThree(a, b, c));
 
-	        // Test case 2: Max string at 2nd position
-	        a = "Apple";
-	        b = "Peach";
-	        c = "Banana";
-	        System.out.println("Maximum of \"" + a + "\", \"" + b + "\", \"" + c + "\" is \"" + MaximumOfThreeString(a, b, c) + "\"");
-
-	        // Test case 3: Max string at 3rd position
-	        a = "Apple";
-	        b = "Banana";
-	        c = "Peach";
-	        System.out.println("Maximum of \"" + a + "\", \"" + b + "\", \"" + c + "\" is \"" + MaximumOfThreeString(a, b, c) + "\"");
+	        // Test case 3: Max number at 3rd position
+	        a = 5;
+	        b = 3;
+	        c = 10;
+	        System.out.println("Maximum of " + a + ", " + b + ", " + c + " is " + MaximumOfThree(a, b, c));
+	    }
+	        
 			
-		}
 
 }
