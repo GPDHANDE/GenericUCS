02e15f61fa01a80d4adea39cbccdbc95ed56a423
Gaurav <gpdhande12@gmail.com>
2024-06-28 15:07:52 +0530

    find maximum for n no of parameters

--- MaxOfThree.java
+++ MaxOfThree.java
@@ -1,26 +1,21 @@
 package org.genericus;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MaxOfThree<T extends Comparable<T>>
 {
-	private  T a;
-	private T b;
-	private T c;
 	
-	
+	public static <T extends Comparable <T>> T Max(T... elements)
+	{
+		if(elements == null || elements.length==0)
+		{
+			throw new IllegalArgumentException("At least one element is required");
+		}
+		Arrays.sort(elements);
 		
-	public MaxOfThree(T a, T b, T c) 
-	{
-		this.a = a;
-		this.b = b;
-		this.c = c;
+		return elements[elements.length-1];
 	}
-
-
-	public T testMaximum()
-	{
-		return testMaximum(a,b,c);
-	}
-	
 	public static <T extends Comparable > T testMaximum(T a,T b,T c)
 	{
 		T max = a;
@@ -38,17 +33,17 @@ public class MaxOfThree<T extends Comparable<T>>
 	public static void main(String[] args) 
 	{
 		//Test case 1: Max int
-		MaxOfThree<Integer> m = new MaxOfThree<Integer>(87, 113, 191);
-		System.out.println("Maximum of " + m.a + ", " + m.b + ", " + m.c + " is "+m.testMaximum());
+		System.out.println("Maximum of int is "+Max(10,20,30,40,50));
 		
 		//Test case 2: Max float
-		MaxOfThree<Float> floatFinder = new MaxOfThree<>(10.5f, 5.2f, 3.1f);
-        System.out.println("Maximum of " + floatFinder.a + ", " + floatFinder.b + ", " + floatFinder.c + " is " + floatFinder.testMaximum());
+		System.out.println("Maximum of float is "+Max(10.34f,73.2f,23.4f,43.7f,54.90f));
+		
 
         // Test case 3: Max string
-        MaxOfThree<String> stringFinder = new MaxOfThree<>("zelda", "apple", "banana");
-        System.out.println("Maximum of \"" + stringFinder.a + "\", \"" + stringFinder.b + "\", \"" + stringFinder.c + "\" is \"" + stringFinder.testMaximum() + "\"");
-		
+      
+		String[] words = {"apple", "banana", "pear", "kiwi"};
+        System.out.println("Maximum of String is "+Max(words));
+        
     }
 	        
 			
